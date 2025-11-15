07c7b75c65df390bca24e445982dcce71279dd0d
Nishant <onenish14@gmail.com>
2025-11-15 09:44:02 +0530

    package

--- index.ts
+++ index.ts
@@ -2,8 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import http from "http";
-import app from "./app";
-import connectDB from "./config/db";
+import app from "./app.js";
+import connectDB from "./config/db.js";
+
 
 const PORT = Number(process.env.PORT || 4000);
 
